806855f29dfd6fda2158e6b423799300ca0049a9
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-04-24 08:19:24 +0300

    KTB-14 Parsing a string using regular expressions (RegEx) V2

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -10,25 +10,29 @@ fun main(args: Array<String>) {
     val botToken = args[0]
     var updateId = 0
 
-    while (true) {
+    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+    val updateIdRegex = "\"update_id\"".toRegex()
 
+    while (true) {
         Thread.sleep(2000)
+
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        println(updateIdString)
-        updateId = updateIdString.toInt() + 1
+        val updateIdMatch = updateIdRegex.find(updates)
+        val currentUpdateId = updateIdMatch?.groups?.get(1)?.value?.toIntOrNull()
 
-        val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        if (currentUpdateId != null) {
+            updateId = currentUpdateId + 1
+        }
+
+
         val matchResult: MatchResult? = messageTextRegex.find(updates)
-        val groups = matchResult?.groups
-        val text = groups?.get(1)?.value
-        println(text)
+        val text = matchResult?.groups?.get(1)?.value
 
+        if (text != null) {
+            println("Получено сообщение: $text")
+        }
     }
 }
 
